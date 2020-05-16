2f5fd8fbd5172def5955481bbb9acf1310df7907
Max <max.hofferlormand@hotmail.fr>
2020-05-16 15:56:24 +0200

    Interpreteur fonctionnel

--- main.c
+++ main.c
@@ -1,31 +1,43 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(void){
 
 		char entry[50];
-		char *com;
-		
+		char *arg[20];
+		int i=0;
 		
 		while(1){
+				
+					
 				printf("Promp command or exit to quit: ");
-				scanf("%s",entry);
+				fgets(entry, 50, stdin);
 				
-				com =strtok(entry," ");
+				char *p = strtok(entry, " ");
 				
-				printf("La commande taper est: ");
-				printf(com);
-				printf("\n");
+				while(p != NULL){
+					arg[i] = p;
+					p = strtok(NULL," ");
+					i++;
+				}
 				
-				if(strcmp("exit",com) == 0){
+				
+				
+				
+				
+				if(strcmp("exit",arg[0]) < 1){
 					printf("sortie \n");
+					exit(0);
+				}
+				else if(strcmp("echo",arg[0]) < 1){
+					printf("echo: %s \n", arg[1]);
 				}
 				else{
-					printf("La suite de la commande est ");
-					printf(entry);
-					printf("\n");
+					printf("entre else\n");
+					
 				}
 				
-				
+				i = 0;
 		}
 }
